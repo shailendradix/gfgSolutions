b473233ca5577edd4cc62f281377b318ce0ff5fb
shailendradix <128203983+shailendradix@users.noreply.github.com>
2025-02-10 16:32:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print adjacency list/print-adjacency-list.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to return the adjacency list for each vertex.
+    vector<vector<int>> printGraph(int V, vector<pair<int, int>>& edges) {
+        // Code here
+        // edges di hain ,hamein graph / adjavency list bananani hai 
+        // adjacency list: ek node se kis kis node par jaa skte ho 
+        
+        vector<vector<int>> adjList(V); // agar pahle se size nhi doge to adj[u] likhte hi segmentation fault de dega 
+        
+        for (auto& edge:edges){
+            int u=edge.first;
+            int v=edge.second; 
+            
+            adjList[u].push_back(v);
+            adjList[v].push_back(u);
+        }
+        return adjList;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<pair<int, int>> edges;
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            edges.push_back({u, v});
+        }
+        Solution obj;
+        vector<vector<int>> adj = obj.printGraph(V, edges);
+        bool empty = true;
+        for (int i = 0; i < V; i++) {
+            if (adj[i].empty())
+                continue;
+            empty = false;
+            break;
+        }
+        if (empty) {
+            cout << "[]\n";
+            continue;
+        }
+        for (int i = 0; i < V; i++) {
+            set<int> st(adj[i].begin(), adj[i].end());
+            cout << "[";
+            auto it = st.begin();
+
+            while (it != st.end()) {
+                cout << *it;
+                if (next(it) != st.end())
+                    cout << " ";
+                ++it;
+            }
+            cout << "]";
+            cout << endl;
+        }
+    }
+}
+// } Driver Code Ends
