5886cdd7396f9326bb9a2518fc0c546ba2f759bf
shailendradix <128203983+shailendradix@users.noreply.github.com>
2025-04-02 15:54:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -5,6 +5,83 @@ using namespace std;
 
 // } Driver Code Ends
 
+
+class Solution {
+  public:
+    vector<int> parent;
+    vector<int> rank;
+
+    int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]); // Path Compression
+    }
+
+    void unionSet(int x, int y) { // Renamed from union to unionSet
+        int x_parent = find(x);
+        int y_parent = find(y);
+        if (x_parent == y_parent) return;
+        if (rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } 
+        else if (rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } 
+        else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+
+    int kruskal(vector<vector<int>>& vec) { 
+        int sum = 0; 
+        for (auto& temp : vec) {
+            int u = temp[0];
+            int v = temp[1]; 
+            int wt = temp[2]; 
+            
+            // ab parent nikal ke check kr lenge ki dono same compo me h ki nhi
+            int parent_u = find(u); // find function from DSU 
+            int parent_v = find(v); 
+            
+            if (parent_u != parent_v) {
+                unionSet(u, v); 
+                // since union kiya means us edge ko le liya
+                sum += wt;
+            }
+        }
+        return sum; 
+    }
+
+    // Function to find sum of weights of edges of the Minimum Spanning Tree.
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        vector<vector<int>> vec;
+        parent.resize(V);
+        rank.resize(V, 0); // parent and rank use in find and unionset in DSU must be initialized 
+
+        for (int i = 0; i < V; i++){
+            parent[i] = i; // DSU me starting me khud khud ka parent sbhi
+        }
+
+        for (int u = 0; u < V; u++){
+            for (auto& temp : adj[u]){
+                int v = temp[0]; 
+                int wt = temp[1]; 
+                vec.push_back({u, v, wt});
+            }
+        }
+        
+        auto comparator = [&](vector<int>& vec1, vector<int>& vec2) {
+            return vec1[2] < vec2[2]; 
+        }; 
+        sort(vec.begin(), vec.end(), comparator);//acc ot wt sort krna h na
+    
+        return kruskal(vec); 
+    }
+};
+
+/*
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
@@ -51,6 +128,7 @@ class Solution {
         
     }
 };
+*/
 
 
 //{ Driver Code Starts.
